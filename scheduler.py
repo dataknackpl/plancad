1ea25fd885851c234dcae58e5acedaac189c9eaf
Piotr Dziuba <piotr.dziuba@dataknack.pl>
2020-04-29 19:31:34 +0200

    add all_combination_scheduler

--- scheduler.py
+++ scheduler.py
@@ -1,21 +1,53 @@
 from typing import Tuple, List, Dict, Callable
+import itertools
 
 Activity = str
 RoomSlot = Tuple[str, str]
+Schedule = Dict[Activity, RoomSlot]
+Constraint = Callable[[Schedule], bool]
 
 
 class SchedulerError(Exception):
     pass
 
 
+def zip_activites_slots(activities, slots):
+    return {a: s for a, s in zip(activities, slots)}
+
+
+def dummy_scheduler(
+    activities: List[Activity], slots: List[RoomSlot], constraints: List[Constraint]
+) -> Schedule:
+    """ The most naive scheduler that totally ignores constraints
+      and zips activities with slots. Rather PoC only """
+    return zip_activites_slots(activities, slots)
+
+
+def all_combination_scheduler(
+    activities: List[Activity], slots: List[RoomSlot], constraints: List[Constraint]
+) -> Schedule:
+    """ Simple scheduler that scans through all combinations
+        and tries return first that satisfy all constraints
+
+        Very nice in concept  but extremally inefficient!
+    """
+    for slot_combination in itertools.combinations(slots, len(activities)):
+        schedule = zip_activites_slots(activities, slot_combination)
+        if all(c(schedule) for c in constraints):
+            return schedule
+    return {}
+
+
 def find_schedule(
-    activities: List[Activity], slots: List[RoomSlot], constraints: List[Callable] = []
-) -> Dict[Activity, RoomSlot]:
+    activities: List[Activity],
+    slots: List[RoomSlot],
+    constraints: List[Constraint] = [],
+) -> Schedule:
     """ Finds a schedule that meets all constraints
         Returns map between Activity and RoomSlots, or empty dict if not possible
     """
     if len(activities) > len(slots):
         raise SchedulerError("Number of activities exceeds slots")
 
-    schedule = {a: s for a, s in zip(activities, slots)}
+    schedule = all_combination_scheduler(activities, slots, constraints)
     return schedule
--- test_scheduler.py
+++ test_scheduler.py
@@ -1,18 +1,10 @@
-import pytest # type: ignore
+import pytest  # type: ignore
 
 from scheduler import find_schedule, SchedulerError
 
 
-def test_find_schedule_no_constraints():
-    """ Given a set of Activities, RoomSlots
-        and no constrains, calling find_schedule
-        should be able to find mapping for all Activities
-    """
-    activities = ["A1", "A2", "A3"]
-    slots = [("R1", "M8"), ("R1", "M9"), ("R2", "M8"), ("R2", "M9")]
-
-    schedule = find_schedule(activities, slots)
-
+def check_basic_properties_of_schedule(activities, slots, schedule):
+    " Verify the invariants of the correct schedule "
     assert len(schedule) == 3
 
     assert set(schedule.keys()) == set(activities), "all activities should be mapped"
@@ -25,8 +17,21 @@ def test_find_schedule_no_constraints():
     ), "slots in solution must be unique"
 
 
+def test_find_schedule_no_constraints():
+    """ Given a set of Activities, RoomSlots
+        and no constrains, calling find_schedule
+        should be able to find mapping for all Activities
+    """
+    activities = ["A1", "A2", "A3"]
+    slots = [("R1", "M8"), ("R1", "M9"), ("R2", "M8"), ("R2", "M9")]
+
+    schedule = find_schedule(activities, slots)
+
+    check_basic_properties_of_schedule(activities, slots, schedule)
+
+
 def test_find_schedule_not_enough_slots():
-    """ Given a set of Activities, and to few RoomSlots
+    """ Given a set of Activities and to few RoomSlots
         calling find_schedule
         should raise exception
     """
@@ -35,3 +40,26 @@ def test_find_schedule_not_enough_slots():
 
     with pytest.raises(SchedulerError):
         find_schedule(activities, slots)
+
+
+def test_find_schedule_single_constraint():
+    """ given a set of Activities and RoomSlots
+        and a constraint function that check if given activity
+        is placed in exact room, we should get a solution
+        that schedules that activity into that room
+    """
+    activities = ["A1", "A2", "A3"]
+    slots = [("R1", "M8"), ("R1", "M9"), ("R2", "M8"), ("R2", "M9")]
+
+    def a2_constraint(schedule):
+        for activity, slot in schedule.items():
+            if activity == "A2" and slot[0] != "R2":
+                return False
+        return True
+
+    schedule = find_schedule(activities, slots, [a2_constraint])
+    check_basic_properties_of_schedule(activities, slots, schedule)
+
+    # check if constraint is met on the result schedule
+    print(f"{schedule=}")
+    assert a2_constraint(schedule), "schedule should match given constraints"
