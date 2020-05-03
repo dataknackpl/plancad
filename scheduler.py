08985f1ba1ce7bfcecd8c8d053b4e80f5fd6544a
Piotr Dziuba <piotr.dziuba@dataknack.pl>
2020-05-03 21:53:30 +0200

    add naive_search_scheduler

--- scheduler.py
+++ scheduler.py
@@ -1,5 +1,6 @@
 from typing import Tuple, List, Dict, Callable
 import itertools
+from collections import deque
 
 Activity = str
 RoomSlot = Tuple[str, str]
@@ -38,6 +39,48 @@ def all_combination_scheduler(
     return {}
 
 
+def naive_search_scheduler(
+    activities: List[Activity], slots: List[RoomSlot], constraints: List[Constraint]
+) -> Schedule:
+    """ Scheduler which uses Deep First Search aproach """
+
+    def get_next_unasigned_variable(schedule):
+        for activity in activities:
+            if activity not in schedule:
+                return activity
+        return None
+
+    def get_possible_values(schedule, next_variable):
+        used_values = set(schedule.values())
+        return [v for v in slots if v not in used_values]
+
+    queue: deque = deque()
+    queue.append(dict())
+    visited = set()
+
+    while queue:
+        schedule = queue.popleft()
+        if frozenset(schedule.items()) in visited:
+            continue
+
+        if not all(c(schedule) for c in constraints):
+            visited.add(frozenset(schedule.items()))
+            continue
+
+        next_variable = get_next_unasigned_variable(schedule)
+        if not next_variable:
+            return schedule  # We have a winner!
+
+        possible_values = get_possible_values(schedule, next_variable)
+        if possible_values:
+            for value in reversed(possible_values):
+                queue.appendleft({**schedule, next_variable: value})
+
+        visited.add(frozenset(schedule.items()))
+
+    return {}  # No solution found
+
+
 def find_schedule(
     activities: List[Activity],
     slots: List[RoomSlot],
@@ -49,5 +92,6 @@ def find_schedule(
     if len(activities) > len(slots):
         raise SchedulerError("Number of activities exceeds slots")
 
-    schedule = all_combination_scheduler(activities, slots, constraints)
+    # schedule = all_combination_scheduler(activities, slots, constraints)
+    schedule = naive_search_scheduler(activities, slots, constraints)
     return schedule
